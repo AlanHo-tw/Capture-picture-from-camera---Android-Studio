89b16aca2478b15c972812f3dc49d6c13c8c6e51
AlanHo137 <wakubila@gmail.com>
2024-05-12 19:35:21 +0800

    Before turning on the camera to take pictures, delete the previous photos.

--- app/src/main/java/com/example/imagehandling/ImageViewActivity.java
+++ app/src/main/java/com/example/imagehandling/ImageViewActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.util.Printer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,6 +16,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -70,6 +73,9 @@ public class ImageViewActivity extends AppCompatActivity {
     }
 
     private void openCamera() {
+        File directory = getExternalFilesDir(null);
+        deleteJPGFiles(directory);
+
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (cameraIntent.resolveActivity(getPackageManager()) != null) {
             File photoFile = null;
@@ -78,12 +84,14 @@ public class ImageViewActivity extends AppCompatActivity {
             } catch (IOException ignored) {}
 
             if (photoFile != null) {
-                photoUri = FileProvider.getUriForFile(ImageViewActivity.this,"com.example.imagehandling.fileprovider", photoFile);
+                photoUri = FileProvider.getUriForFile(ImageViewActivity.this,
+                        "com.example.imagehandling.fileprovider", photoFile);
                 cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
                 startActivityForResult(cameraIntent, CAMERA_REQUEST);
             }
         }
     }
+
 
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -95,6 +103,7 @@ public class ImageViewActivity extends AppCompatActivity {
                 storageDir
         );
         currentPhotoPath = image.getAbsolutePath();
+        Log.d("MyApp","currentPhotoPath: " + currentPhotoPath);
         return image;
     }
 
@@ -144,5 +153,22 @@ public class ImageViewActivity extends AppCompatActivity {
             return null;
         }
     }
+
+    public void deleteJPGFiles(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list(new FilenameFilter() {
+                // Apply filter on each file in directory
+                // Return true if file name ends with .jpg or .JPG
+                public boolean accept(File dir, String name) {
+                    return (name.endsWith(".jpg") || name.endsWith(".JPG"));
+                }
+            });
+
+            for (String child : children) {
+                new File(dir, child).delete();
+            }
+        }
+    }
+
 
 }
